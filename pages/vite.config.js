82744d67b29fe6a82c7c13394800c9188f0ec719
chance <chance.edwards1@outlook.com>
2025-05-25 18:30:23 -0500

    config updates

--- pages/vite.config.js
+++ pages/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// pages/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: '/Edwards-Library-/'   // <- repo name with trailing slash
+});
