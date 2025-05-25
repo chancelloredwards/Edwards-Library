e082e31341870a8778d38c02448b6115c4307ded
chance <chance.edwards1@outlook.com>
2025-05-25 18:37:56 -0500

    edits

--- pages/vite.config.js
+++ pages/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/Edwards-Library-/'   // <- repo name with trailing slash
+    base: '/Edwards-Library/'   // <- repo name with trailing slash
 });
